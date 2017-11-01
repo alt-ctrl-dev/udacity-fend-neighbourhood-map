6719e767594276022d8af5459d7cfbcfb586d7f8
Coutinho, Reuben <reuben.coutinho@accenture.com>
2017-11-01 11:52:40 +1100

    Refactor: pointing map to local neighbourhood

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
-function initMap(){
-alert("init")
+let map;
+function initMap() {
+  // Constructor creates a new map - only center and zoom are required.
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -37.8141, lng: 144.9633},
+    zoom: 15
+  });
 }
